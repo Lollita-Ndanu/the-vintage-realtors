f0aa1edaa24a23f19daad00664ea1e3889c3454c
Lollita-Ndanu <lollindanu@gmail.com>
2026-02-01 17:51:31 +0300

    CMS: frontmatter properties, add generator and index, fix admin redirects

---
+++ scripts/generate-properties-index.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+
+const propertiesDir = path.join(__dirname, '..', 'properties');
+const outFile = path.join(propertiesDir, 'index.json');
+
+function main(){
+  const files = fs.readdirSync(propertiesDir)
+    .filter(f => f.endsWith('.md'))
+    .sort();
+
+  const all = files.map(filename => ({ filename }));
+  fs.writeFileSync(outFile, JSON.stringify({ all }, null, 2), 'utf8');
+  console.log('Wrote', outFile);
+}
+
+main();
